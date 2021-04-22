4e3cdff44a427a1ef2c421025da9e843445fc51a
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-22 16:31:49 +0200

    initial commit

---
+++ components/logo.php
@@ -0,0 +1,6 @@
+<div class="logo__wrapper">
+  <svg width="40" height="80" viewBox="0 0 40 80" fill="none" xmlns="http://www.w3.org/2000/svg" class="logo">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M39 0H1V38H39V0ZM20 26C23.3137 26 26 23.3137 26 20C26 16.6863 23.3137 14 20 14C16.6863 14 14 16.6863 14 20C14 23.3137 16.6863 26 20 26Z" fill="#161616"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M20 80C31.0457 80 40 71.0457 40 60C40 48.9543 31.0457 40 20 40C8.9543 40 0 48.9543 0 60C0 71.0457 8.9543 80 20 80ZM25 55H15V65H25V55Z" fill="#161616"/>
+  </svg>
+</div>
---
+++ components/mainHeader.php
@@ -0,0 +1,36 @@
+<div class="mainHeader__wrapper">
+  <header class="mainHeader">
+    <?php @include 'logo.php'; ?>
+    <nav class="mainHeader__nav">
+      <ul class="mainHeader__list">
+        <li class="mainHeader__item">
+          <span class="mainHeader__item__title">Festiwal</span>
+          <ul class="mainHeader__submenu">
+            <li class="mainHeader__submenu__item">Aktualności</li>
+            <li class="mainHeader__submenu__item">Organizatorzy</li>
+            <li class="mainHeader__submenu__item">Zespół festiwalu</li>
+            <li class="mainHeader__submenu__item">Biogramy</li>
+          </ul>
+        </li>
+        <li class="mainHeader__item">Idea</li>
+        <li class="mainHeader__item">
+          <span class="mainHeader__item__title">Archiwum</span>
+          <ul class="mainHeader__submenu">
+            <li class="mainHeader__submenu__item">Edycja I: Blizny Historii 2007</li>
+            <li class="mainHeader__submenu__item">Edycja II: Wykluczeni 2009</li>
+            <li class="mainHeader__submenu__item">Edycja III: Święta rodzina 2010</li>
+            <li class="mainHeader__submenu__item">Edycja IV: Herosi i Heroiny 2011</li>
+            <li class="mainHeader__submenu__item">Edycja V: Przechodnie 2012</li>
+            <li class="mainHeader__submenu__item">Edycja VI: Ojcowie 2013</li>
+          </ul>
+        </li>
+        <li class="mainHeader__item">Partnerzy festiwalu</li>
+        <li class="mainHeader__item">Kontakt</li>
+      </ul>
+    </nav>
+    <button class="mainHeader__language">PL</button>
+  </header>
+  <button class="button button--primary">
+    Bilety
+  </button>
+</div>
