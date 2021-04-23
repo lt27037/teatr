d94edb67442cbcd2cda4bd701939af55787c1ed8
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-23 14:15:56 +0200

    create biography page

---
+++ bio.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php @include './bodyElements/head.php' ?>
+  <link rel="stylesheet" href="./styles/bio.css">
+  <title>Teatr Polski w Poznaniu | Biogramy</title>
+</head>
+<body>
+  <?php @include './components/mainHeader.php' ?>
+  <main class="container">
+    <header class="container__header">
+      <h2 class="container__title">Program Festiwalu</h2>
+    </header>
+    <section class="bioSection">
+      <h3 class="bioSection__title">Koincydencja</h3>
+      <article class="bioSection__element">
+        <h4 class="bioSection__element__title">Yuliia Andriichuk </h4>
+        <p class="bioSection__element__content">Artystka sztuk wizualnych i nowych mediów oraz fotografka urodzona w Żytomierzu w Ukrainie. Absolwentka Uniwersytetu Artystycznego w Poznaniu – mieście, w którym mieszka od czasu rozpoczęcia studiów. Współzałożycielka fundacji Axis Cactus oraz wytwórni płytowej Axis Cactus Records. Współpracuje m.in. z FRIV Festival i poznańskim Festiwalem Słuchowisk. Współorganizatorka projektu kulturalnego „Miejscownik”. Laureatka konkursu im. Marii Dokowicz.</p>
+      </article>
+      <article class="bioSection__element">
+        <h4 class="bioSection__element__title">Patryk Lichota</h4>
+        <p class="bioSection__element__content">Muzyk, kompozytor, artysta multimedialny, producent muzyczny, kurator. Autor muzyki teatralnej, słuchowisk, twórca instalacji multimedialnych (m.in. Cyborganic Plants, Omnihype, Oda di Neuro, Laseroom). Absolwent Kulturoznawstwa i Muzykologii UAM w Poznaniu. Doktor nauk o sztuce, wykładowca na kierunku Media Interaktywne i Widowiska na UAM, w Akademii Sztuki w Szczecinie oraz na kierunku Intermedia Uniwersytetu Artystycznego w Poznaniu. Członek centrum badawczego UAM HAT Center. Dyrektor artystyczny i producent festiwalu muzyki improwizowanej FRIV. Współorganizator Festiwalu Słuchowisk. Kurator Mediations Biennale. Kompozytor muzyki do filmów The Case of Josephine H. (2014) oraz Trucizna, 16 (2017) i Hybryda (2018) w reż. Izabelli Gustowskiej. Twórca niezależnych i eksperymentalnych spektakli Strange Lóóp, Lightskin, Audfit, Echo Clickers. Laserflesh, Kserotyka Nielotów. W swojej pracy naukowej zajmuje się wirtualną rzeczywistością i teleobecnością, cybereschatologią, interaktywnością, sound art i sound design, teorią nowych mediów i gier wideo. Autor książki Tradycje hałasu w sztuce dźwięku.</p>
+      </article>
+    </section>
+    <section class="bioSection">
+      <h3 class="bioSection__title">Marmur społeczny</h3>
+      <article class="bioSection__element">
+        <h4 class="bioSection__element__title">Sergey Shabohin</h4>
+        <p class="bioSection__element__content">Urodzony w 1984. Jeden z najwybitniejszych białoruskich twórców młodego pokolenia, artysta sztuk wizualnych, kurator, scenograf i krytyk. Założyciel i redaktor naczelny portalu dedykowanego białoruskiej sztuce współczesnej i teatrowi „Art Aktivist” oraz platformy badawczej Kalektar. Od 2016 roku żyje w Polsce, od 2020 w Poznaniu. Pracuje zarówno w Polsce, jak i w Białorusi, ale także w Niemczech, Ukrainie, Czechach. Wśród jego najważniejszych wystaw indywidualnych z ostatnich lat można wymienić: Sergey Shabohin: Social Marble in the Center of Europe, OK16, Mińsk, Białoruś / 2019; Social Marble in the Center of Europe, Wessen Europa? Whose Europe?; CLB Berlin, Niemcy / 2019; Exhibition Sergey Shabohin: 18 / Practices of Subordination, Canteen XYZ, Mińsk, Bialoruś / 2018 (za wystawę artysta został nominowany do Rada Award. Best Art Project 2018).</p>
+      </article>
+    </section>
+    <section class="bioSection">
+      <h3 class="bioSection__title">Przysposobienie obronne</h3>
+      <article class="bioSection__element">
+        <h4 class="bioSection__element__title">Dmytro Levytskyi</h4>
+        <p class="bioSection__element__content">Urodzony w 1986 r. w Komsomolsku ukraiński dramatopisarz, reżyser, artysta konceptualny. Obecnie mieszka w Kijowie. Absolwent Chmielnickiego Uniwersytetu Narodowego (tytuł magistra filologii ukraińskiej) i prestiżowej Szkoły Dramatycznej w Moskwie. Współzałożyciel grupy Pic Pic realizującej interaktywne wycieczki audio. Jego zainteresowania koncentrują się wokół przestrzeni miejskiej w jej społecznym, ekonomicznym i historycznym wymiarze. Swoje projekty realizował dla Centrum Badań nad Kulturą Wizualną w Kijowie, Festiwalu Filmu i Urbanistyki 86 w Sławutyczu (Ukraina), Instytutu Teatralnego im. Zbigniewa Raszewskiego w Warszawie. W 2019 był kuratorem projektu Kijowska w ramach Biennale Warszawa. Jest autorem dramaturgii do głośnego ukraińskiego spektaklu Restoran Ukraina (2017) koprodukowanego przez Goethe-Institut, którego tematem jest korupcja i system patriarchalny w Ukrainie.</p>
+      </article>
+      <article class="bioSection__element">
+        <h4 class="bioSection__element__title">Jakub Skrzywanek </h4>
+        <p class="bioSection__element__content">Urodzony w 1992 roku we Wrocławiu. Reżyser, autor, twórca instalacji performatywnych, absolwent Wydziału Reżyserii Dramatu w Akademii Sztuk Teatralnych w Krakowie oraz Filologii Polskiej na Uniwersytecie Wrocławskim. W 2013 roku w ramach stypendium studiował także na Uniwersytecie Karola w Pradze. W styczniu 2016 roku oficjalnie zadebiutował spektaklem Cynkowi chłopcy zrealizowanym w Teatrze Dramatycznym im. Jerzego Szaniawskiego w Wałbrzychu, który został pokazany na najważniejszych polskich festiwalach. W kolejnych latach współpracował z Teatrem Powszechnym w Warszawie, Teatrem Polskim w Poznaniu, Teatrem Muzycznym Capitol we Wrocławiu, Teatrem im. Jana Kochanowskiego w Opolu, Teatrem Zagłębia w Sosnowcu, Teatrem Współczesnym w Szczecinie, czy Teatrem 21, dla którego – wraz z Justyną Sobczyk – wyreżyserował Superspektakl przygotowany w ramach koprodukcji z Teatrem Powszechnym w Warszawie. Jego najgłośniejsze spektakle to Kordian (w Teatrze Polskim w Poznaniu) oraz Mein Kampf (w Teatrze Powszechnym w Warszawie), przygotowany w ramach obchodów Roku Antyfaszystowskiego, gdzie po raz pierwszy w historii na scenie teatralnej podjęto bezpośrednią polemikę z książką autorstwa Adolfa Hitlera, co odbiło się szerokim echem nie tylko w Polsce, ale też poza granicami, gdzie spektakl Skrzywanka był bardzo pozytywnie odbierany m. in. przez „Financial Times”, „New York Times”, „Der Freitag”, „Jerusalem Post”, czy „L’Espresso”. W 2019 roku w miesięczniku „Dialog” ukazał się jego debiutancki dramat Pogrom alfonsów, Warszawa 1905, który został zakwalifikowany do półfinału Gdyńskiej Nagrody Dramaturgicznej. Ostatnio (2019) wyreżyserował w Teatrze Współczesnym w Szczecinie znakomicie przyjętego przez krytykę Kaspara Hausera.</p>
+      </article>
+    </section>
+    <section class="bioSection">
+      <h3 class="bioSection__title">Ten spacer</h3>
+      <article class="bioSection__element">
+        <h4 class="bioSection__element__title">Piotr Armianovski</h4>
+        <p class="bioSection__element__content">Urodził się w 1985 roku w Doniecku. Jest absolwentem Politechniki Donieckiej – w swoich projektach łączy sztukę i technologię, zajmuje się filmem i sztukami performatywnymi. Studiował także w donieckiej School of Performance przy Teatrze Zhuki oraz był uczestnikiem mistrzowskich warsztatów Mariny Abramović w 2011 roku. W 2019 roku był współuczestnikiem prezentacji Pawilonu Ukraińskiego na Biennale w Wenecji (Cień „Marzenia” padający na Giardini della Biennale). Jego debiutancki projekt VR był prezentowany na Sheffield Doc Fest 2019 i IDFAcademy w Wielkiej Brytanii. W ciągu ostatnich lat Armianovski wyprodukował, niezależnie łącząc rolę producenta i reżysera, osiem krótkometrażowych filmów dokumentalnych o wydarzeniach i ludziach z regionu Doniecka, gdzie wojna trwa od pięciu lat. Jego filmy były pokazywane między innymi na IHRFF Docudays UA, DocuBak, Cottbus Film Festival, 86 IFFU i Artdocfest, a dokument Ja i Mariupol znajduje się w kolekcji Muzeum Ludwiga w Budapeszcie. Film Musztarda w ogrodach otrzymał specjalne wyróżnienie i nagrodę Andrija Matrosowa na Docudays UA (2018) oraz główną nagrodę na II Biennale Młodej Sztuki (2019). W 2016 roku wraz z dramaturgiem Dimą Levytskyim Armianovski stworzył grupę artystyczną Pic Pic. W 2020 roku otrzymał stypendium Gaude Polonia przyznawane przez polskie Ministerstwo Kultury i Dziedzictwa Narodowego.</p>
+      </article>
+    </section>
+  </main>
+  <?php @include './components/footer.php' ?>
+</body>
+</html>
