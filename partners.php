f597eae0ec215a1a914988d84542a924d2e34cca
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-23 15:37:13 +0200

    create partners page

---
+++ partners.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php @include './bodyElements/head.php' ?>
+  <link rel="stylesheet" href="./styles/partners.css">
+  <title>Teatr Polski w Poznaniu | Partnerzy</title>
+</head>
+<body>
+  <?php @include './components/mainHeader.php' ?>
+  <main class="container">
+    <header class="container__header">
+      <h2 class="container__title">Partnerzy Festiwalu</h2>
+    </header>
+    <section class="partnersSection">
+      <h3 class="partnersSection__title">Organizatorzy</h3>
+      <div class="partnersSection__wrapper">
+        <img 
+        src="./images/fundacja-reszka-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+        <img 
+        src="./images/teatr-polski-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+      </div>
+    </section>
+    <section class="partnersSection">
+      <h3 class="partnersSection__title">Współorganizatorzy</h3>
+      <div class="partnersSection__wrapper">
+        <img 
+        src="./images/poznan-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+        <img 
+        src="./images/ukrainian-institute-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+        <img 
+        src="./images/instytut-teatralny-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+        <img 
+        src="./images/ministerstwo-kultury-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+      </div>
+    </section>
+    <section class="partnersSection">
+      <h3 class="partnersSection__title">Współorganizatorzy</h3>
+      <div class="partnersSection__wrapper">
+        <img 
+        src="./images/giacomo-conti-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+        <img 
+        src="./images/bilety-24-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+        <img 
+        src="./images/solor-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+        <img 
+        src="./images/ams-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+        <img 
+        src="./images/buswifi-logo.svg" 
+        alt="Logo partnera festiwalu" 
+        class="partnersSection__logo">
+      </div>
+    </section>
+  </main>
+  <?php @include './components/footer.php' ?>
+</body>
+</html>
