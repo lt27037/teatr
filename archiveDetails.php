180346b7e828e8c035e9060876918e73a003d532
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-23 18:52:17 +0200

    create archiveDetiles page

---
+++ archiveDetails.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php @include './bodyElements/head.php' ?>
+  <link rel="stylesheet" href="./styles/archiveDetails.css">
+  <title>Teatr Polski w Poznaniu | Archiwum</title>
+</head>
+<body>
+  <?php @include './components/mainHeader.php' ?>
+  <main class="container">
+    <header class="container__header">
+      <h2 class="container__title">Jazda obowiązkowa</h2>
+    </header>
+    <section class="details">
+      <div class="details__leftBar">
+        <div class="details__element">
+          <span class="details__element__title">kiedy</span>
+          <span class="details__element__content">9 / 11 / 2020</span>
+        </div>
+        <div class="details__element">
+          <span class="details__element__title">godzina</span>
+          <span class="details__element__content">17:00</span>
+        </div>
+        <div class="details__element">
+          <span class="details__element__title">miejsce</span>
+          <span class="details__element__content">KINO MUZA, sala nr. 1</span>
+        </div>
+        <div class="details__element">
+          <span class="details__element__title">scenariusz i reżyseria</span>
+          <span class="details__element__content">Ewa Kochańska</span>
+        </div>
+        <div class="details__element">
+          <span class="details__element__title">produkcja</span>
+          <span class="details__element__content">Magdalena Borowiec/SQUARE film studio ltd. (Polska)</span>
+        </div>
+        <div class="details__element">
+          <span class="details__element__title">premiera</span>
+          <span class="details__element__content">listopad 2018</span>
+        </div>
+        <div class="details__element">
+          <span class="details__element__title">czas trwania</span>
+          <span class="details__element__content">72 minuty</span>
+        </div>
+        <div class="details__element">
+          <span class="details__element__title">cena</span>
+          <span class="details__element__content">Wydarzenie już się odbyło</span>
+        </div>
+      </div>
+      <div class="details__rightBar">
+        <p class="details__text">10-letnia Julia Polniuk trenuje łyżwiarstwo figurowe. Ma coraz mniej czasu, aby odnieść sukces i spełnić oczekiwania swoich bliskich. Po wybuchu Euromajdanu jej rodzina wyemigrowała do Polski. Decyzję o przeprowadzce podjęła mama Julii, Marina. W dwa tygodnie spakowała walizki, wypisała dzieci ze szkoły, a nowo wyremontowane mieszkanie wystawiła na sprzedaż. Teraz rodzina Polniuków stara się o obywatelstwo polskie i rozpoczyna całkiem nowe życie w Warszawie. Bez pracy, zabezpieczenia finansowego i dobrej znajomości języka. Marina rzuca losowi wyzwanie, szukając w Polsce lepszych perspektyw na przyszłość dla swoich dzieci. Zrobi wszystko, aby miały lepsze życie niż ona. Największe nadzieje pokłada właśnie w Julii. W tym roku dziewczynka ma szansę wystartować w Młodzieżowych Mistrzostwach Polski. A za tym idzie sukces, uznanie i początek kariery sportowej. Julia próbuje sprostać oczekiwaniom mamy, ale jest jej bardzo trudno w nowym kraju, w nowej szkole, pod okiem wymagającej trenerki. Czy będzie w stanie rozwinąć skrzydła, niosąc ciężar cudzych ambicji i oczekiwań…?</p>
+      </div>
+    </section>
+    <nav class="details__nav">
+      <button class="details__nav__button button--prev">Poprzednie</button>
+      <button class="details__nav__button button--next">Następne</button>
+    </nav>
+  </main>
+  <?php @include './components/footer.php' ?>
+</body>
+</html>
