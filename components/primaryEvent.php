87f7309466ff2a928234192011a84bbf6c3d440a
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-23 11:49:35 +0200

    create program page

---
+++ components/primaryEvent.php
@@ -0,0 +1,38 @@
+<article class="primaryEvent">
+  <img 
+  src="./images/event-picture.png" 
+  alt="Zdjęcie eventu" 
+  class="primaryEvent__picture">
+  <section class="primaryEvent__container">
+    <span class="primaryEvent__date">3 / 20 / 2021</span>
+    <h3 class="primaryEvent__title">KOINCYDENCJA | instalacja 
+    multimedialna</h3>
+    <div class="primaryEvent__wrapper">
+      <div class="primaryEvent__info">
+        <span class="primaryEvent__info__title">miejsce</span>
+        <span class="primaryEvent__info__content">TEATR POLSKI</span>
+      </div>
+      <div class="primaryEvent__info">
+        <span class="primaryEvent__info__title">artyści</span>
+        <span class="primaryEvent__info__content">Yuliia Andriichuk, Patryk Lichota</span>
+      </div>
+    </div>
+    <div class="primaryEvent__wrapper">
+      <div class="primaryEvent__info">
+        <span class="primaryEvent__info__title">kiedy</span>
+        <span class="primaryEvent__info__content">22 / 10 / 2020</span>
+      </div>
+      <div class="primaryEvent__info">
+        <span class="primaryEvent__info__title">tekst</span>
+        <span class="primaryEvent__info__content">Dima Levytskyi i zespół Piękne Kwiaty z Kijowa</span>
+      </div>
+    </div>
+      <div class="primaryEvent__info">
+        <span class="primaryEvent__info__title">godzina</span>
+        <span class="primaryEvent__info__content">18:30</span>
+      </div>
+    <button class="primaryEvent__button" title="Zobacz więcej">
+      Zobacz więcej
+    </button>
+  </section>
+</article>
---
+++ program.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php @include './bodyElements/head.php' ?>
+  <link rel="stylesheet" href="./styles/program.css">
+  <title>Teatr Polski w Poznaniu | Program Festiwalu</title>
+</head>
+<body>
+  <?php @include './components/mainHeader.php' ?>
+  <main class="container">
+    <header class="container__header">
+      <h2 class="container__title">Program Festiwalu</h2>
+    </header>
+    <section class="container__wrapper">
+      <?php @include './components/primaryEvent.php' ?>
+      <?php @include './components/primaryEvent.php' ?>
+      <?php @include './components/primaryEvent.php' ?>
+    </section>
+    <button class="button button--ghost">Wczytaj więcej</button>
+  </main>
+  <?php @include './components/footer.php' ?>
+</body>
+</html>
