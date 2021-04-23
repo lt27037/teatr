2c520480ba791d60cca55ae33414792bd191a6a0
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-23 12:25:04 +0200

    create news page

---
+++ news.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php @include './bodyElements/head.php' ?>
+  <link rel="stylesheet" href="./styles/news.css">
+  <title>Teatr Polski w Poznaniu | Aktualnie</title>
+</head>
+<body>
+  <?php @include './components/mainHeader.php' ?>
+  <main class="container">
+    <header class="container__header">
+      <h2 class="container__title">Aktualnie</h2>
+    </header>
+    <section class="container__wrapper">
+      <div class="container__wrapper__column">
+        <?php @include './components/primaryArticle.php' ?>
+        <?php @include './components/primaryArticle.php' ?>
+        <?php @include './components/primaryArticle.php' ?>
+        <?php @include './components/primaryArticle.php' ?>
+      </div>
+      <div class="container__wrapper__column">
+        <?php @include './components/primaryArticle.php' ?>
+        <?php @include './components/primaryArticle.php' ?>
+        <?php @include './components/primaryArticle.php' ?>
+        <?php @include './components/primaryArticle.php' ?>
+        <?php @include './components/primaryArticle.php' ?>
+      </div>
+    </section>
+    <button class="button button--ghost">Wczytaj więcej</button>
+  </main>
+  <?php @include './components/footer.php' ?>
+</body>
+</html>
