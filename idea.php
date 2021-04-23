1c5277b2908ba9b9b97f30af1fa02f3d88e0159b
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-23 15:13:10 +0200

    create idea page

---
+++ idea.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php @include './bodyElements/head.php' ?>
+  <link rel="stylesheet" href="./styles/idea.css">
+  <title>Teatr Polski w Poznaniu | Idea</title>
+</head>
+<body>
+  <?php @include './components/mainHeader.php' ?>
+  <main class="container">
+    <header class="container__header">
+      <h2 class="container__title">Idea</h2>
+    </header>
+    <section class="ideaSection">
+      <h3 class="ideaSection__title">XIII Spotkania Teatralne Bliscy Nieznajomi: Ukraina</h3>
+      <div class="ideaSection__leftBar">
+        <span class="ideaSection__element">
+          <span class="ideaSection__element__title">nazwa wydarzenia</span>
+          <span class="ideaSection__element__content">Jeśli nie ty, to kto?</span>
+        </span>
+        <span class="ideaSection__element">
+          <span class="ideaSection__element__title">kiedy</span>
+          <span class="ideaSection__element__content">22–27 października, 26 listopada 2020</span>
+        </span>
+        <span class="ideaSection__element">
+          <span class="ideaSection__element__title">organizatorzy</span>
+          <span class="ideaSection__element__content">Teatr Polski w Poznaniu i Fundacja Reszka</span>
+        </span>
+        <span class="ideaSection__element">
+          <span class="ideaSection__element__title">współorganizatorzy</span>
+          <span class="ideaSection__element__content">Miasto Poznań, Instytut Ukraiński w Kijowie, Ministerstwo Kultury i Dziedzictwa Narodowego, Instytut Teatralny im. Zbigniewa Raszewskiego w Warszawie
+          </span>
+        </span>
+      </div>
+      <div class="ideaSection__rightBar">
+        <p class="ideaSection__paragraph">W dzisiejszej Polsce, także w Poznaniu, żyjemy z Ukrainkami i Ukraińcami w jednej wspólnocie. Po raz pierwszy od czasów powojennych stajemy się społeczeństwem wielokulturowym. Pytanie jednak, czy wiemy o sobie wystarczająco dużo, czy raczej powtarzamy stereotypy. Z tego też powodu Spotkania Teatralne Bliscy Nieznajomi – w ostatnich latach prezentujące jedynie polski teatr i sztukę – poszerzyły formułę i od zeszłego roku można zobaczyć w ramach festiwalu także spektakle oraz projekty artystek i artystów ukraińskich.</p>
+        <p class="ideaSection__paragraph">Tegoroczną edycję Bliskich Nieznajomych wyznaczają pytania. Jedno z nich wybrzmiewało w czasie rewolucji na Majdanie: Jeśli nie ty, to kto?, pozostałe postawimy w trakcie festiwalu:</p>
+        <ul class="ideaSection__list">
+          <li class="ideaSection__list__item">Kto ma dawać nadzieję?</li>
+          <li class="ideaSection__list__item">Kto ma budować wspólnotę?</li>
+          <li class="ideaSection__list__item">Kto ma mówić o tym, co bolesne?</li>
+          <li class="ideaSection__list__item">Kto ma ufać?</li>
+          <li class="ideaSection__list__item">Kto ma „robić miłość, a nie wojnę”?</li>
+        </ul>
+        <p class="ideaSection__paragraph">Podczas prezentacji, spotkań i debat będziemy pytać twórców i publiczność o to, kto mówi i w czyim imieniu. Do kogo należy opowieść, czy raczej wiele opowieści, widzianych z różnych pozycji oraz kształtowanych przez indywidualne i zarazem zbiorowe doświadczenia. Choć będziemy rozmawiać także o bolesnych wydarzeniach z przeszłości, to bardzo ważna będzie dla nas również rozmowa o możliwościach budowania nowego, lepszego świata ponad wspólnotami narodowymi, klasami i podziałami.</p>
+        <p class="ideaSection__paragraph">Wydarzenia festiwalowe przybiorą w tym roku rozmaite formy – spektaklu, czytania i wykładu performatywnego, audio-spaceru, multimedialnej instalacji, filmu dokumentalnego.</p>
+        <p class="ideaSection__paragraph">Pokażemy przedstawienie Więzi Teatru Wybrzeże w Gdańsku, na podstawie tekstu i w reżyserii Oleny Apczel, dla którego punktem wyjścia jest Majdan i „rewolucja godności”. Apczel jest jedną z czołowych reprezentantek nowego ukraińskiego teatru, a jednocześnie młodą kobietą, która ma za sobą doświadczenie hybrydowej wojny w Ukrainie (była wolontariuszką na froncie i organizowała pomoc dla ofiar konfliktu), a dziś dzieli swoje życie pomiędzy Ukrainą a Polską. Temat wojny wybrzmi także w czytaniu performatywnym tekstu Przysposobienie obronne autorstwa Dimy Levytsky’ego i znanego charkowskiego kolektywu teatralnego Piękne Kwiaty. Reżyser Jakub Skrzywanek, zaproszona do współpracy ukraińska aktorka Vera Popova i zespół aktorski Teatru Współczesnego w Szczecinie spróbują odpowiedzieć na pytanie, kto i w jaki sposób ma prawo mówić o dramatycznych wydarzeniach wojny, która jest prowadzona za naszymi granicami.</p>
+        <p class="ideaSection__paragraph">Projektując festiwal zależało nam na tym, by zainicjować współpracę artystyczną pomiędzy ukraińskimi i polskimi artystami, także tymi mieszkającymi w Poznaniu. Wierzymy, że zrealizowane przez nich premierowe projekty będą owocować twórczymi przyjaźniami i kolejnymi wspólnymi realizacjami.</p>
+        <p class="ideaSection__paragraph">Pierwszym wyprodukowanym w ramach festiwalu projektem będzie Ten spacer zainicjowany przez kijowski kolektyw teatralny z Kijowa Pic Pic, którego twórcami są Piotr Armianovski i Dima Levytskyi. Głównym medium, z którym pracują artyści jest spacer performatywny / audio-walk: publiczność podąża za przewodnikami słuchając jednocześnie narratora przez słuchawki. Użycie takiego medium sprawia, że codzienne przestrzenie, oswojone i zdefiniowane miejsca, odkrywają inne znaczenia – ukryte, zatarte, wymazywane, niewygodne. Tematem, który artyści będą badać i opracowywać artystycznie w ramach festiwalu jest doświadczanie miasta przez kobiety. Za warstwę muzyczną wydarzenia odpowiadać będzie Patryk Lichota. W ramach tego projektu powstanie także film dokumentalny (w reżyserii Magdaleny Mosiewicz) oraz odbędzie się debata o sytuacji i prawach kobiet w Polsce, a w szczególności w Poznaniu.</p>
+        <p class="ideaSection__paragraph">Druga festiwalowa premiera to wykład performatywny Republika Głuchych, który opierać się będzie na poetyckiej opowieści Deaf Republic autorstwa Ilii Kaminskiego – wybitnego poety, laureata Anisfield-Wolf Book Award (2020), wykładowcy akademickiego, krytyka, a jednocześnie osoby dotkniętej niepełnosprawnością słuchową. Wykład zawierać będzie fragmenty wierszy performowane przez głuchego artystę Daniela Kotowskiego, aktorkę Martynę Zarembę-Mąćkowiak, oryginalne nagrania poety oraz komentarze Anny Łazar – tłumaczki i krytyczki, przywołujące wybrane wątki z kultury współczesnej Europy Wschodniej. W ramach festiwalu publiczność doświadczy także multimedialnej instalacji Koincydencja stworzonej przez ukraińsko-polskie małżeństwo artystów młodego pokolenia mieszkające w Poznaniu – Yulię Andriichuk i Patryka Lichotę. Twórcy zaproszą publiczność do świata na styku kultur, by zobaczyć (i usłyszeć) to, co kulturowo nas łączy i odróżnia, jak i to, co jest nam bliskie i dalekie.</p>
+        <p class="ideaSection__paragraph">Choć głównym idiomem Bliskich Nieznajomych jest Ukraina, to w tym roku bardzo ważne jest dla nas także wyrażenie solidarności z Białorusią. Dlatego naszym gościem będzie jeden z najważniejszych artystów młodego pokolenia z tego kraju – mieszkający od niedawna w Poznaniu – Sergey Shabohin. Twórca stworzy instalację dedykowaną białoruskiej rewolucji.</p>
+        <span class="ideaSection__author">Agata Siwiak /dyrektorka artystyczna, kuratorka/Joanna Wichowska /kuratorka/</span>
+      </div>
+    </section>
+  </main>
+  <?php @include './components/footer.php' ?>
+</body>
+</html>
