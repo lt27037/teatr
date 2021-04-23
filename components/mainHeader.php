cfdefacca806c62ff90d944fca5bd3f8ebfcc7be
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-23 16:39:18 +0200

    add links to menu

--- components/mainHeader.php
+++ components/mainHeader.php
@@ -4,28 +4,32 @@
     <nav class="mainHeader__nav">
       <ul class="mainHeader__list">
         <li class="mainHeader__item">
-          <span class="mainHeader__item__title">Festiwal</span>
+          <a href="news.php" class="mainHeader__item__title">Festiwal</a>
           <ul class="mainHeader__submenu">
-            <li class="mainHeader__submenu__item">Aktualności</li>
-            <li class="mainHeader__submenu__item">Organizatorzy</li>
-            <li class="mainHeader__submenu__item">Zespół festiwalu</li>
-            <li class="mainHeader__submenu__item">Biogramy</li>
+            <a href="news.php"><li class="mainHeader__submenu__item">Aktualności</li></a>
+            <a href="organizer.php"><li class="mainHeader__submenu__item">Organizatorzy</li></a>
+            <a href="crew.php"><li class="mainHeader__submenu__item">Zespół festiwalu</li></a>
+            <a href="bio.php"><li class="mainHeader__submenu__item">Biogramy</li></a>
           </ul>
         </li>
-        <li class="mainHeader__item">Idea</li>
+        <li class="mainHeader__item"><a href="idea.php">Idea</a></li>
         <li class="mainHeader__item">
           <a href="archive.php" class="mainHeader__item__title">Archiwum</a>
           <ul class="mainHeader__submenu">
-            <li class="mainHeader__submenu__item">Edycja I: Blizny Historii 2007</li>
-            <li class="mainHeader__submenu__item">Edycja II: Wykluczeni 2009</li>
-            <li class="mainHeader__submenu__item">Edycja III: Święta rodzina 2010</li>
-            <li class="mainHeader__submenu__item">Edycja IV: Herosi i Heroiny 2011</li>
-            <li class="mainHeader__submenu__item">Edycja V: Przechodnie 2012</li>
-            <li class="mainHeader__submenu__item">Edycja VI: Ojcowie 2013</li>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja I: Blizny Historii 2007</li></a>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja II: Wykluczeni 2009</li></a>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja III: Święta rodzina 2010</li></a>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja IV: Herosi i Heroiny 2011</li></a>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja V: Przechodnie 2012</li></a>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja VI: Ojcowie 2013</li></a>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja VII: Kain i Abel 2014</li></a>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja VIII: One 2015</li></a>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja XI: POLSKA 2018</li></a>
+            <a href="archive.php"><li class="mainHeader__submenu__item">Edycja XII: Ukraina 2019</li></a>
           </ul>
         </li>
-        <li class="mainHeader__item">Partnerzy festiwalu</li>
-        <li class="mainHeader__item">Kontakt</li>
+        <li class="mainHeader__item"><a href="partners.php">Partnerzy festiwalu</a></li>
+        <li class="mainHeader__item"><a href="contact.php">Kontakt</a></li>
       </ul>
     </nav>
     <button class="mainHeader__language">PL</button>
