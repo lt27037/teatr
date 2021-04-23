0268363fe74893fbe41476452833f91581535f57
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-23 13:52:10 +0200

    create crew page

---
+++ crew.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php @include './bodyElements/head.php' ?>
+  <link rel="stylesheet" href="./styles/crew.css">
+  <title>Teatr Polski w Poznaniu | Zespół</title>
+</head>
+<body>
+  <?php @include './components/mainHeader.php' ?>
+  <main class="container">
+    <section class="crew">
+      <header class="crew__header">
+        <h2 class="crew__title">Zespół festiwalu</h2>
+      </header>
+      <div class="crew__leftBar">
+        <div class="crew__element">
+          <span class="crew__name">dyrektorka artystyczna festiwalu</span>
+          <span class="crew__content">Agata Siwiak</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">kuratorki</span>
+          <span class="crew__content">Joanna Wichowska, Agata Siwiak</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">współpraca programowa i organizacyjna</span>
+          <span class="crew__content">Anastasiia Haishenets, Nadia Sokolenko (Ukrainian Institute)</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">producentka</span>
+          <span class="crew__content">Agata Siwiak</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">asystentka produkcji</span>
+          <span class="crew__content">Zuzanna Kupczyk</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">kierownik techniczny</span>
+          <span class="crew__content">Andrzej Szwaczyk</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">redakcja folderu</span>
+          <span class="crew__content">Anna Maśląkowska</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">prowadzące spotkania</span>
+          <span class="crew__content">Anna Łazar, Agata Siwiak, Joanna Wichowska</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">projekt plakatu i identyfikacji festiwalu</span>
+          <span class="crew__content">Danil Daneliuk</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">tłumaczenie tekstów programowych na język ukraiński</span>
+          <span class="crew__content">Lubov Ilnytska</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">media</span>
+          <span class="crew__content">Katarzyna Kamińska</span>
+        </div>
+      </div>
+      <aside class="crew__rightBar">
+        <img src="./images/crew-photo-1.jpg" alt="Zdjęcie zespołu" class="crew__photo">
+        <img src="./images/crew-photo-2.jpg" alt="Zdjęcie zespołu" class="crew__photo">
+      </aside>
+    </section>
+    <section class="crew">
+      <header class="crew__header">
+        <h2 class="crew__title">Zespół artystyczny</h2>
+      </header>
+      <div class="crew__leftBar">
+        <div class="crew__element">
+          <span class="crew__name">w sezonie 2020 / 2021</span>
+          <span class="crew__content">Mariusz Adamski, Konrad Cichoń,  Piotr B. Dąbrowski, Michał Kaleta, Wojciech Kalwat, Piotr Kaźmierczak, Barbara Krasińska, Teresa Kwiatkowska, Jakub Papuga, Monika Roszko, Michał Sikorski, Paweł Siwiak, Alona Szostak, Andrzej Szubski, Ewa Szumska, Kornelia Trawkowska, Wiesław Zanowicz</span>
+        </div>
+        <div class="crew__element">
+          <span class="crew__name">inspicjentka </span>
+          <span class="crew__content">Magdalena Matusewicz</span>
+        </div>
+      </div>
+      <aside class="crew__rightBar">
+      </aside>
+    </section>
+  </main>
+  <?php @include './components/footer.php' ?>
+</body>
+</html>
