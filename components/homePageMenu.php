1ab54bee796ba32a6056bca009e6f888d61be343
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-04-26 16:05:48 +0200

    add some patches from trello

--- components/homePageMenu.php
+++ components/homePageMenu.php
@@ -27,5 +27,13 @@
         </li>
         <li class="homePageMenu__item"><a href="partners.php">Partnerzy festiwalu</a></li>
         <li class="homePageMenu__item"><a href="contact.php">Kontakt</a></li>
+        <span class="mouseArrows">
+          <svg width="26" height="54" viewBox="0 0 26 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20.5833 8.64C20.5833 4.46473 17.1882 1.08 13 1.08C8.81185 1.08 5.41667 4.46473 5.41667 8.64V17.28C5.41667 21.4553 8.81185 24.84 13 24.84C17.1882 24.84 20.5833 21.4553 20.5833 17.28V8.64Z" stroke="#161616" stroke-width="2"/>
+            <path id="scroll" d="M14.0833 6.75C14.0833 6.00442 13.5983 5.4 13 5.4C12.4017 5.4 11.9167 6.00442 11.9167 6.75V9.45C11.9167 10.1956 12.4017 10.8 13 10.8C13.5983 10.8 14.0833 10.1956 14.0833 9.45V6.75Z" fill="#161616"/>
+            <path id="arrow-bottom" d="M5.41667 35.64L13 43.2L20.5833 35.64" stroke="#161616" stroke-width="1.5"/>
+            <path id="arrow-top" d="M5.41667 29.16L13 36.72L20.5833 29.16" stroke="#161616" stroke-width="1.5"/>
+          </svg>
+        </span>
       </ul>
     </nav>
--- index.php
+++ index.php
@@ -11,7 +11,13 @@
     <section class="topSection">
       <?php @include './components/homePageMenu.php' ?>
       <img src="./images/topSection-baner.png" alt="Baner" class="topSection__banner">
-      <img src="./images/pointer.svg" alt="pointer icon" class="topSection__mobileIcon">
+      <span class="topSection__mobileIcon">
+        <svg width="24" height="50" viewBox="0 0 24 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11.9995 15.5C11.9995 16.3285 11.1055 17 10.2992 17C9.49285 17 9 16.3285 9 15.5V14.5C9 15.3285 8.18582 16 7.3795 16C6.57319 16 6 15.3285 6 14.5V13V13.531C6 14.3595 5.26615 15.031 4.45983 15.031C3.65352 15.031 3 14.3595 3 13.531V8.49998C3 4.35798 6.26808 0.999984 10.2992 0.999985C12.3147 0.999985 14.1395 1.83949 15.4607 3.19649L20.6844 9.95549C21.1443 10.5505 21.0966 11.405 20.574 11.9425C20.0056 12.5265 19.0844 12.5265 18.5161 11.9425L16.4602 9.82999C15.8027 9.15449 15 9.633 15 10.588V13V21.5C15 22.3285 14.3465 23 13.5402 23C12.7338 23 12 22.3285 12 21.5V14.5" stroke="#161616" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+          <path id="arrow-top" d="M4 25L11.5833 32.56L19.1667 25" stroke="#161616" stroke-width="1.5"/>
+          <path id="arrow-bottom" d="M4 31L11.5833 38.56L19.1667 31" stroke="#161616" stroke-width="1.5"/>
+        </svg>
+      </span>
     </section>
     <section class="newsSection">
       <header class="newsSection__header">
