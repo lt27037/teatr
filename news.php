92a64914c9bcce831264dbf7f05e65c35249aa2f
Łukasz Tukindorf <lukasz.tukindorf@gmail.com>
2021-05-06 17:11:13 +0200

    change flex order in news wrapper

--- news.php
+++ news.php
@@ -12,22 +12,32 @@
       <h2 class="container__title">Aktualnie</h2>
     </header>
     <section class="news">
-      <div class="news__column">
-        <?php @include './components/primaryArticle.php' ?>
-        <?php @include './components/primaryArticle.php' ?>
-        <?php @include './components/primaryArticle.php' ?>
-        <?php @include './components/primaryArticle.php' ?>
-      </div>
-      <div class="news__column">
-        <?php @include './components/primaryArticle.php' ?>
-        <?php @include './components/primaryArticle.php' ?>
-        <?php @include './components/primaryArticle.php' ?>
-        <?php @include './components/primaryArticle.php' ?>
-        <?php @include './components/primaryArticle.php' ?>
-      </div>
+      <?php @include './components/primaryArticle.php' ?>
+      <?php @include './components/primaryArticle.php' ?>
+      <?php @include './components/primaryArticle.php' ?>
+      <?php @include './components/primaryArticle.php' ?>
+      <?php @include './components/primaryArticle.php' ?>
+      <?php @include './components/primaryArticle.php' ?>
+      <?php @include './components/primaryArticle.php' ?>
+      <?php @include './components/primaryArticle.php' ?>
+      <?php @include './components/primaryArticle.php' ?>
     </section>
     <button class="button button--ghost">Wczytaj więcej</button>
   </main>
   <?php @include './components/footer.php' ?>
+  <script>
+    const posts = document.getElementsByClassName('primaryArticles');
+    const wrapper = document.querySelector('.news');
+
+    // posts.onChange = () => {
+    //   wrapper.style.height = `${math.floor(wrapper.length / 4) * 898}px`
+    // };
+
+    // posts.addEventListener('change', () => {
+    //   console.log(posts.length);
+    // })
+    console.log(posts)
+
+  </script>
 </body>
 </html>
